973f12906b96d607b0244963260acf0de2b429fd
lika1702 <Lika.Fleur@yandex.by>
2022-02-16 12:36:04 +0300

    Создана основа проекта

---
+++ src/main/java/course/groupofgroups/GroupOfGroupsApplication.java
@@ -0,0 +1,13 @@
+package course.groupofgroups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupOfGroupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupOfGroupsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/course/groupofgroups/GroupOfGroupsApplicationTests.java
@@ -0,0 +1,13 @@
+package course.groupofgroups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupOfGroupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
